64462eba6485e20a7fa44e67cbaed94fe92544c1
lichao <frank.li@lixinchuxing.com>
2019-03-22 16:52:59 +0800

    添加注释

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,7 @@ Page({
     topDistAfterScroll: '',
   },
   onButtonClick() {
+    // 该API或去元素相对于显示窗口的位置，top属性为距离显示窗口顶部的高度距离
     this.createSelectorQuery().select('#input3')
         .boundingClientRect()
         .exec((res) => {
@@ -12,6 +13,7 @@ Page({
         })
   },
   onInput3Focus() {
+    // 获取焦点后，延迟一秒后再获取元素位置，保证键盘完全弹起
     setTimeout(() => {
       this.createSelectorQuery().select('#input3')
           .boundingClientRect()
