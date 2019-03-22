0a4ae7b2cda852ee1ec8ab26cedd9f22405e455b
lichao <frank.li@lixinchuxing.com>
2019-03-22 16:48:50 +0800

    验证键盘弹起，视口是否发生移动

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,23 @@
 //index.js
-Page({})
+Page({
+  data: {
+    topDistBeforeScroll: '',
+    topDistAfterScroll: '',
+  },
+  onButtonClick() {
+    this.createSelectorQuery().select('#input3')
+        .boundingClientRect()
+        .exec((res) => {
+          this.setData({ topDistBeforeScroll: res[0].top })
+        })
+  },
+  onInput3Focus() {
+    setTimeout(() => {
+      this.createSelectorQuery().select('#input3')
+          .boundingClientRect()
+          .exec((res) => {
+            this.setData({ topDistAfterScroll: res[0].top })
+          })
+    }, 1000)
+  }
+})
