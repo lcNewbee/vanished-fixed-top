baeac233d16333bfa45563f6467de0793f5f6168
lichao <frank.li@lixinchuxing.com>
2019-03-25 11:38:42 +0800

    simple solution

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,19 @@
 //index.js
-Page({})
+Page({
+  data: {
+    isInput3Focus: false
+  },
+  onInputTap(e) {
+    wx.pageScrollTo({
+      scrollTop: 600,
+      duration: 300,
+      complete: () => {
+        this.setData({ isInput3Focus: true })
+      }
+    })
+  },
+  onInputBlur(e) {
+    // 失去焦点后要保证输入框是禁用状态
+    this.setData({ isInput3Focus: false })
+  }
+})
