966204921d94b89d9f6c5b8a81d9477f620a1085
lichao <frank.li@lixinchuxing.com>
2019-03-26 16:48:36 +0800

    三号输入框滚动处理完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,16 +4,63 @@ Page({
     isInput3Focus: false
   },
   onInputTap(e) {
-    wx.pageScrollTo({
-      scrollTop: 600,
-      duration: 300,
-      complete: () => {
+    const id = e.target.id
+    Promise.all([
+      this.calcElemPosById(id),
+      this.calcViewportPos(),
+      this.calcViewportHeight()
+    ]).then(([
+      inputRectPos,
+      pageScrollTop,
+      viewportHeight
+    ]) => {
+      // 假设键盘不超过视口高度的一半，则当输入框在页面上半部分，不滚动
+      if (inputRectPos.bottom < viewportHeight / 2) {
         this.setData({ isInput3Focus: true })
+        return
       }
+      // 滚动位置 = 输入框距离视口顶部距离 + 页面已经滚动的距离 - 顶部栏的高度 - 滚动后输入框和顶部栏的距离
+      const scrollTop = inputRectPos.top + Math.abs(pageScrollTop) - 66 - 20
+      wx.pageScrollTo({
+        scrollTop,
+        duration: 300,
+        complete: () => {
+          this.setData({ isInput3Focus: true })
+        }
+      })
     })
   },
   onInputBlur(e) {
     // 失去焦点后要保证输入框是禁用状态
     this.setData({ isInput3Focus: false })
+  },
+  // 计算输入框位置
+  calcElemPosById(id) {
+    return new Promise((resolve, reject) => {
+      wx.createSelectorQuery()
+        .select(`#${id}`)
+        .boundingClientRect((rect) => {
+          resolve(rect)
+        }).exec()
+    })
+  },
+  // 计算页面滚动位置
+  calcViewportPos() {
+    return new Promise((resolve, reject) => {
+      wx.createSelectorQuery()
+        .selectViewport()
+        .scrollOffset((res) => {
+          resolve(res.scrollTop)
+        }).exec()
+    })
+  },
+  calcViewportHeight() {
+    return new Promise((resolve, reject) => {
+      wx.createSelectorQuery()
+        .selectViewport()
+        .boundingClientRect((rect) => {
+          resolve(rect.height)
+        }).exec()
+    })
   }
 })
